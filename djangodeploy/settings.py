b68d8c41f273cc9cc703a2744dbf1324c4e23f7a
MSTLAUR <mstlaur@gmail.com>
2025-02-03 21:09:32 -0500

    ENGINE

--- djangodeploy/settings.py
+++ djangodeploy/settings.py
@@ -84,10 +84,13 @@ WSGI_APPLICATION = 'djangodeploy.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL', default=''),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        **dj_database_url.config(
+            default=config('DATABASE_URL', default=''),
+            conn_max_age=600
+        )
+    }
 }
 
 # Password validation
